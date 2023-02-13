3e634a40f678c706eeba378f5ec9577c96915361
TrungVu <vutrung26072001@gmail.com>
2023-02-14 03:23:29 +0700

    create Product Nav Footer component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,9 +1,17 @@
 import ProductsContext from '../context/ProductProvider'
 import { useContext } from 'react'
+import Product from './Product'
 const Products = () => {
   const { products } = useContext(ProductsContext)
   console.log(products)
-  return <div className="bg-red-500">Products</div>
+  return (
+    <div className="pt-20 mx-2 sm:mx-auto max-w-5xl grid grid-cols-1 md:grid-cols-3 gap-3">
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+    </div>
+  )
 }
 
 export default Products
