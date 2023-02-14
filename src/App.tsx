d26d8fe65e0fa2ff396ee7bd46b580325758503d
TrungVu <vutrung26072001@gmail.com>
2023-02-14 23:13:49 +0700

    👍 Small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Products from './components/Products'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App() {
   return (
-    <div>
-      <div className="text-red-500">Hello</div>
+    <div className=" dark:bg-gray-600">
+      <Navbar />
       <Products />
+      <Footer />
     </div>
   )
 }
